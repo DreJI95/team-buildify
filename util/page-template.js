d13d7f8f78490047516153d28525d1269c2896d7
Andre M <andre.ji.moseley@gmail.com>
2021-07-11 14:12:55 -0400

    resubmitted with urls

--- util/page-template.js
+++ util/page-template.js
@@ -52,7 +52,7 @@ const generateHtmlDoc = function (employeeList) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TeamProfileGenerator</title>
+    <title>TeamBuildify</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
   </head>
